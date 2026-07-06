1f8b40ff22fe4932e39fa19630dfd89ca64c84bf
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2026-07-06 03:50:16 -0500

    feat: add migration

--- Backend-DispenXCore.Api/Shared/Extensions/MigrationExtensions.cs
+++ Backend-DispenXCore.Api/Shared/Extensions/MigrationExtensions.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Backend_DispenXCore.Api.Infrastructure.Persistence;
 
 namespace Backend_DispenXCore.Api.Shared.Extensions;
+
 public static class MigrationExtensions
 {
     public static void ApplyMigrations(this WebApplication app)
     {
+        // Se crea un scope para obtener servicios registrados en el contenedor de dependencias.
         using var scope = app.Services.CreateScope();
+        // Se obtiene el contexto de base de datos de la aplicación.
         var db = scope.ServiceProvider.GetRequiredService<DispenXDbContext>();
+        // Aplica automáticamente las migraciones pendientes a la base de datos.
         db.Database.Migrate();
     }
 }
