3c688653164a80ffedac74c6d4ef3f13c71ab863
alomsoo <alonoso.percy@gmail.com>
2026-07-07 13:58:09 -0500

    fix: allow anonymous access to dispenser-events endpoint for Edge/ESP32 integration

--- Backend-DispenXCore.Api/obj/Debug/net8.0/Backend-DispenXCore.Api.AssemblyInfo.cs
+++ Backend-DispenXCore.Api/obj/Debug/net8.0/Backend-DispenXCore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend-DispenXCore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8b40ff22fe4932e39fa19630dfd89ca64c84bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d14f7722a0bf5708404b47e8a609a3197b0b66f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend-DispenXCore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend-DispenXCore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
